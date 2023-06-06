adacd890922bb16b2b83c9f399f818ba27bc19f2
Shayan Shabani <120071557+shayanshabani711@users.noreply.github.com>
2023-06-05 17:08:58 -0700

    Add files via upload

---
+++ constant_components.py
@@ -0,0 +1,17 @@
+width = 720
+height = 720
+
+rows = 3
+columns = 3
+
+sqr_size = 240
+line_color = (0, 0, 0)
+line_width = 20
+
+circ_color = (20, 100, 50)
+radius = sqr_size // 3
+circ_width = 20
+
+cross_color = (50, 100, 20)
+cross_width = 20
+line_dist = 40
